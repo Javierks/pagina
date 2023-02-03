a8514ba3b27bd5df992083d94673079bb9764f33
Javier Bermeo <jafebesa@gmail.com>
2023-02-02 19:28:12 -0500

    repositorio javier

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOm from "react-dom/client";
+import "../src/estilos.css";
+import horizonte from "../src/img/horizonte.jpg";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOm.createRoot(document.getElementById("root"))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function pagina(){
+    return <div>
+        
+        <div class="contenedor-rojo">
+            <a>INICIO</a>
+
+            <div class="primero">
+            <article><img src={horizonte}/></article>
+                
+
+
+<article>
+
+
+<h1>INFORMACIÓN</h1>
+<h2>NOMBRE:  JAVIER FELIPE BERMEO SALZAR</h2>
+<h2>CARRERA: INGENIERÍA DE SOFTWARE</h2>
+<h2>UNIVERSIDAD: HORIZONTE </h2>
+<h2>TELÉFONO: 3188832777</h2>
+<h2>DIRECCIÓN: CARRERA 50 #163 B 80</h2>  
+
+</article>
+
+
+</div>
+<div class="contenedor-azul"></div>
+        </div>
+    </div>
+    
+    
+}
+    root.render(pagina())
+    
+        
+
+
+
